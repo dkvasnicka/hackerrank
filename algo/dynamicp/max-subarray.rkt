3806aba7c307638a39682f67a28e7b89fe0a27a8
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2015-03-22 20:58:12 +0100

    max subarray - brute force

--- algo/dynamicp/max-subarray.rkt
+++ algo/dynamicp/max-subarray.rkt
@@ -1,26 +1,44 @@
 #lang racket
 
+(require racket/unsafe/ops)
+
+(define (intermediate-sums ary)
+  (let loop ([xs ary]
+             [i 0])
+    (if (null? xs)
+      empty-stream
+      (let ([v (unsafe-fx+ i (car xs))])
+        (stream-cons v (loop (cdr xs) v))))))
+
+(define (stream-max s)
+  (stream-fold unsafe-fxmax (stream-first s) (stream-rest s)))
+
 (define (max-subary-sum ary)
-  0)
+  (inexact->exact
+    (let loop ([mx -inf.0]
+               [subary ary])
+      (if (null? subary)
+        mx
+        (loop (max mx (stream-max (intermediate-sums subary)))
+              (cdr subary))))))
 
 (define (max-nonctg-sum ary)
-  (call-with-values
-    (λ ()
-       (for/fold ([mx -10e5]
-                  [sum 0]
-                  [all-negative? #t])
-         ([i ary])
-         (values
-           (max mx i)
-           (if (positive? i) (+ sum i) sum)
-           (and all-negative? (negative? i)))))
-    (λ (mx sum all-negative?)
-       (if all-negative? mx sum))))
+  (let-values 
+    ([(mx sum all-negative?)
+      (for/fold ([mx (inexact->exact -10e5)]
+                 [sum 0]
+                 [all-negative? #t])
+                ([i ary])
+                (values
+                  (unsafe-fxmax mx i)
+                  (if (positive? i) (unsafe-fx+ sum i) sum)
+                  (and all-negative? (negative? i))))])
+     (if all-negative? mx sum)))
 
 (let ([cases (read)])
   (for ([_ (range cases)])
     (let* ([size (read)]
-           [ary (build-list size (λ (_) (read)))])
+           [ary (build-list size (lambda (_) (read)))])
       (display (max-subary-sum ary))
       (display " ")
       (displayln (max-nonctg-sum ary)))))
