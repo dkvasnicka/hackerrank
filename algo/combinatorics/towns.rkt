6d7de02d63facfb2934863e1f49ba90e2ad8747f
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2015-03-23 21:17:09 +0100

    gandalf going to rivendell

---
+++ algo/combinatorics/towns.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(require unstable/sequence)
+
+(let ([_ (read-line)])
+  (for ([tc (in-slice 2 (in-lines))])
+       (displayln
+         (modulo
+           (sequence-fold * 1 
+                          (in-port read 
+                                   (open-input-string (cadr tc))))
+           1234567)))) 
