3677bcc9eff797de03741cc3a8885b2c7e0fbcfc
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2015-03-28 16:34:39 +0100

    even tree - final solution

--- algo/graph-theory/even-tree.rkt
+++ algo/graph-theory/even-tree.rkt
@@ -1,30 +1,45 @@
 #lang racket
 
-(require unstable/sequence)
+(require unstable/sequence
+         math/array
+         srfi/43)
+
+(define (mark-edge G e re state)
+  (array-set! G e state) 
+  (array-set! G re state))
 
 (define (build-graph size edges)
-  (let ([G (make-vector (add1 size) (seteq))])
-    (for ([(a b) edges])
-      (vector-set! G b (set-add (vector-ref G b) a)))
+  (let ([G (array->mutable-array (make-array (vector size size) #f))])
+    (for ([(e re) edges]) 
+      (mark-edge G e re #t)) 
     G))
 
-(define (tree-size G in)
-  (+ (set-count in)
-     (for/sum ([node in])
-       (tree-size G (vector-ref G node)))))
+(define (tree-size G root excl)
+  (for/sum ([idx (in-naturals)]
+            [node (in-array (array-axis-ref G 0 root))]
+            #:when (and (not (= idx excl)) node))
+    (add1 (tree-size G idx root))))
+
+(define (stream-of-edges)
+  (sequence->stream 
+    (sequence-map 
+      (compose
+        (lambda (e) (values 
+                      (list->vector e) 
+                      (reverse-list->vector e)))
+        (curry map sub1)) 
+      (in-slice 2 (in-port)))))
+
+(define (both-subtrees-even? G e re)
+  (for/and ([x e]
+            [y re]) 
+    (even? (add1 (tree-size G x y)))))
 
 (let* ([N (read)]
        [M (read)]
-       [edges (sequence->stream 
-                (sequence-map 
-                  (curry apply values) 
-                  (in-slice 2 (in-port))))]
+       [edges (stream-of-edges)]
        [G (build-graph N edges)])
-  (for/sum ([(a b) edges] 
-            #:when (andmap (compose even? add1 (curry tree-size G))
-                           (list
-                             (vector-ref G a)
-                             (set-remove (vector-ref G b) a))))
-    (vector-set! G b (set-remove (vector-ref G b) a))
+  (for/sum ([(e re) edges] 
+            #:when (both-subtrees-even? G e re))
+    (mark-edge G e re #f)
     1))
-
