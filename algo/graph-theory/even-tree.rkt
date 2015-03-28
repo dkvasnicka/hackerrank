05c0cef8dddd64cadbd0eefae62d665cdf650781
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2015-03-28 14:11:12 +0100

    even tree - adjacency list based solution; partially correct

--- algo/graph-theory/even-tree.rkt
+++ algo/graph-theory/even-tree.rkt
@@ -2,17 +2,29 @@
 
 (require unstable/sequence)
 
-(define (build-graph size port-seq)
-  (vector->immutable-vector
-    (let ([G (make-vector (add1 size) '())])
-      (for ([edge (in-slice 2 port-seq)])
-        (match-let ([(list a b) edge])
-          (vector-set*! G 
-                        a (cons b (vector-ref G a))
-                        b (cons a (vector-ref G b)))))
-      G)))
+(define (build-graph size edges)
+  (let ([G (make-vector (add1 size) (seteq))])
+    (for ([(a b) edges])
+      (vector-set! G b (set-add (vector-ref G b) a)))
+    G))
+
+(define (tree-size G in)
+  (+ (set-count in)
+     (for/sum ([node in])
+       (tree-size G (vector-ref G node)))))
 
 (let* ([N (read)]
        [M (read)]
-       [G (build-graph N (in-port))])
-  G)
+       [edges (sequence->stream 
+                (sequence-map 
+                  (curry apply values) 
+                  (in-slice 2 (in-port))))]
+       [G (build-graph N edges)])
+  (for/sum ([(a b) edges] 
+            #:when (andmap (compose even? add1 (curry tree-size G))
+                           (list
+                             (vector-ref G a)
+                             (set-remove (vector-ref G b) a))))
+    (vector-set! G b (set-remove (vector-ref G b) a))
+    1))
+
