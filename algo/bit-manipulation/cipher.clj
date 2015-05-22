ffca5244d7041deb537e685a73f0f779df3acd35
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2015-05-22 13:16:57 +0200

    Cipher - correct but still not fast enough

--- algo/bit-manipulation/cipher.clj
+++ algo/bit-manipulation/cipher.clj
@@ -1,23 +1,34 @@
 (require 'clojure.string)
 
-(let [size (read)
-      shift (read)
-      _ (read-line)
-      input (map (comp #(Integer/parseInt %) str) (read-line))
-      output (list (first input))]
+(defn phase1 [in out shift]
+  (reduce bit-xor 
+          (second in) 
+          (take-last (dec shift) out)))
+
+(defn phase2 [in out shift]
+  (let [lastout (peek out)]
+    (bit-xor lastout 
+             (nth out (- (count out) shift)) 
+             (first in) 
+             lastout 
+             (second in))))
+
+(let [size 7 ;(read)
+      shift 4 ;(read)
+     ; _ (read-line)
+      input '(1 1 1 0 1 0 0 1 1 0) ;(map (comp #(Integer/parseInt %) str) (read-line))
+      output [(first input)]]
   (println
     (clojure.string/join
-      (reverse
-        (loop [out output
-               counter (dec size)
-               in (rest input)]
-              (if (zero? counter)
-                out
-                (recur 
-                  (cons  
-                    (reduce bit-xor 
-                            (first in) 
-                            (take (dec shift) out))
-                    out)
-                  (dec counter)
-                  (rest in))))))))
+      (loop [out output
+             counter (dec size)
+             in input 
+             phase-fns (lazy-cat (repeat shift phase1) (repeat phase2))]
+            (if (zero? counter)
+              out
+              (recur 
+                (conj out 
+                      ((first phase-fns) in out shift))
+                (dec counter)
+                (rest in)
+                (rest phase-fns)))))))
