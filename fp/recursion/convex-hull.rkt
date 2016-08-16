df8d90b39ba77d1518d7876417499bfd7e14d5da
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-08-16 11:29:27 +0200

    Convex hull perimeter

---
+++ fp/recursion/convex-hull.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+(struct pt (x y) #:transparent)
+
+(define (line->pt l)
+  (apply pt (map string->number (string-split l))))
+
+(define (bottomleft? p1 p2) 
+  (or (< (pt-y p1) (pt-y p2))
+      (and (= (pt-y p1) (pt-y p2))
+           (< (pt-x p1) (pt-x p2)))))
+
+(define (read-points)
+  (for/fold ([bl (line->pt (read-line))]
+             [pts '()])
+            ([nextpt (sequence-map line->pt (in-lines))])
+    (if (bottomleft? nextpt bl)
+      (values nextpt (cons bl pts))
+      (values bl (cons nextpt pts)))))
+
+(define (line-angle p q)
+  (atan (- (pt-y q) (pt-y p))
+        (- (pt-x q) (pt-x p))))
+
+(define (z-coord p q r)
+  (- (* (- (pt-x q) (pt-x p)) (- (pt-y r) (pt-y p)))
+     (* (- (pt-y q) (pt-y p)) (- (pt-x r) (pt-x p)))))
+
+(define (euclidist p q)
+  (sqrt (+ (sqr (- (pt-x p) (pt-x q))) 
+           (sqr (- (pt-y p) (pt-y q))))))
+
+(define (angle-comparator bottomleft p q)
+  (let ([a1 (line-angle bottomleft p)]
+        [a2 (line-angle bottomleft q)])
+    (or (< a1 a2)
+        (and (= a1 a2)
+             (< (euclidist bottomleft p)
+                (euclidist bottomleft q))))))
+
+(define _ (read-line))
+(displayln
+  (let*-values ([(bottomleft points) (read-points)]
+                [(startingset) (sort points (curry angle-comparator bottomleft))]
+                [(p1) (car startingset)]
+                [(p2) (cadr startingset)]) 
+    
+    (let loop ([perimeter (+ (euclidist bottomleft p1)
+                             (euclidist p1 p2))]
+               [candidates (cddr startingset)]
+               [stack (list p2 p1 bottomleft)])
+      
+      (if (empty? candidates)
+        (+ perimeter (euclidist bottomleft (car stack)))
+        (if (negative? (z-coord (cadr stack) (car stack) (car candidates)))
+          (loop (- perimeter (euclidist (cadr stack) (car stack)))
+                candidates
+                (cdr stack))
+          (loop (+ perimeter (euclidist (car stack) (car candidates)))
+                (cdr candidates) 
+                (cons (car candidates) stack)))))))
