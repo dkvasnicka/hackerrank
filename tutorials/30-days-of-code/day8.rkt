c1e7968b899ad31d15fe7b2d6dad23ea01000422
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2017-06-20 14:38:35 +0200

    Better day8 solution

--- tutorials/30-days-of-code/day8.rkt
+++ tutorials/30-days-of-code/day8.rkt
@@ -1,14 +1,13 @@
 #lang racket
 
 (define (build-phonebook) 
-  (for/hash ([i (range (string->number (read-line)))]
-             [l (in-producer read-line)])
-    (apply values (string-split l))))
+  (for/hasheq ([i (in-range (read))])
+    (values (read) (read))))
 
 (let ([phonebook (build-phonebook)])
-  (for ([query (in-lines)])
+  (for ([query (in-port)])
     (displayln
       (let ([maybe-phone (hash-ref phonebook query #f)])
         (if maybe-phone
-          (format "~a=~a" query maybe-phone)
-          "Not found")))))
+            (format "~a=~a" query maybe-phone)
+            "Not found")))))
