f9f474f6b9ef6e7ea1a2766eb482da48af3c9ebd
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2019-02-26 15:39:09 +0100

    Pentagonal numbers

--- fp/memoization/pentagonal-numbers.rkt
+++ fp/memoization/pentagonal-numbers.rkt
@@ -1,17 +1,7 @@
 #lang racket
 
-(define cnt (read))
+(define _ (read))
 
-(define (pentagonal-stream i prev)
-  (let ([p (+ prev (* 2 i) (- i 2))])
-    (stream-cons (list i p) (pentagonal-stream (add1 i) p))))
-
-(define pentagonals
-  (stream-cons '(1 1) (pentagonal-stream 2 1)))
-
-(for ([n (in-port)]
-      [ps pentagonals])
-  (displayln 
-    (if (= n (car ps))
-      (cadr ps)
-      (cadr (stream-ref pentagonals (sub1 n))))))
+(for ([n (in-port)])
+  (displayln
+    (/ (- (* 3 (sqr n)) n) 2)))
